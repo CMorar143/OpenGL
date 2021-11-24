06582d93f4c7aef3807b6ec56e6afefd43bdaa55
Cmorar143 <cian@morar.org>
2021-11-24 14:38:57 +0000

    set up a test framework

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -22,6 +22,8 @@
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 
+#include "tests/TestClearColor.h"
+
 int main(void)
 {
     GLFWwindow* window;
@@ -48,50 +50,10 @@ int main(void)
     glfwSwapInterval(2);
 
     if (glewInit() != GLEW_OK)
-       LOG("Error!");
-
-    float positions[] = {
-        100.0f, 100.0f, 0.0f, 0.0f,
-        400.0f, 100.0f, 1.0f, 0.0f,
-        400.0f, 400.0f, 1.0f, 1.0f,
-        100.0f, 400.0f, 0.0f, 1.0f
-    };
-
-    uint32_t indices[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
+        LOG("Error!");
 
     GLCall(glEnable(GL_BLEND));
     GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-    VertexArray va;
-    VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-
-    VertexBufferLayout layout;
-    layout.Push<float>(2);
-    layout.Push<float>(2);
-    va.AddBuffer(vb, layout);
-
-    IndexBuffer ib(indices, 6);
-
-    glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
-    
-    Shader shader("res/shaders/Basic.shader");
-    shader.Bind();
-    shader.SetUniform4f("u_Color", 0.8f, 0.2f, 0.8f, 1.0f);
-    
-    //Texture texture("res/textures/Niko.png");
-    Texture texture("res/textures/Bloodmoon.png");
-    texture.Bind();
-    shader.SetUniform1i("u_Texture", 0);
-
-    // Unbind everything
-    va.Unbind();
-    vb.Unbind();
-    ib.Unbind();
-    shader.Unbind();
 
     Renderer renderer;
 
@@ -101,75 +63,22 @@ int main(void)
     ImGui::StyleColorsDark();
     ImGui_ImplOpenGL3_Init((char*)glGetString(330));
 
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    test::TestClearColor test;
 
-    glm::vec3 translation(200, 90, 0);
-
-    float r = 0.0f;
-    float increment = 0.05f;
-    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        test.OnUpdate(0.0f);
+        test.OnRender();
 
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-        glm::mat4 mvp = proj * view * model;
-        
-        shader.Bind();
-        shader.SetUniform4f("u_Color", r, 0.2f, 0.8f, 1.0f);
-        shader.SetUniformMat4f("u_MVP", mvp);
-
-        renderer.Draw(va, ib, shader);
-
-        static float f = 0.0f;
-        static int counter = 0;
-
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
-
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when                       edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::End();
-        }
+        test.OnImGuiRender();
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        
-        if (r > 1.0f)
-            increment = -0.05f;
-        else if (r < 0.0f)
-            increment = 0.05f;
 
-        r += increment;
-
-        /* Swap front and back buffers */
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
     }
     //glDeleteProgram(shader);
